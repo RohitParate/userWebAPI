bfa42b41ecfb90a95f0bc1e00b6ca7a6299bcb0f
RohitParate <rohitparate98@gmail.com>
2024-03-19 16:41:33 +0530

    changes in Authentication API for getting users data.

--- UserApplication/Controllers/AuthenticationController.cs
+++ UserApplication/Controllers/AuthenticationController.cs
@@ -1,14 +1,16 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using UserApplication.Entities;
-using UserApplication.Models;
 using UserApplication.Services;
+using UserApplication.Models;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using UserApplication.Entities;
 
 namespace UserApplication.Controllers
 {
@@ -17,48 +19,26 @@ namespace UserApplication.Controllers
     public class AuthenticationController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
-
         private readonly IConfiguration _configuration;
-
         private readonly IMapper _mapper;
-
-
 
         public AuthenticationController(IUserRepository userRepository, IConfiguration configuration, IMapper mapper)
         {
             _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
-            _configuration = configuration ??
-               throw new ArgumentNullException(nameof(configuration));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-
         }
+
         public class AuthenticationRequestBody
         {
             public string UserName { get; set; }
-
             public string Password { get; set; }
         }
 
-        //private class UserInfo
-        //{
-        //    public int Id { get; set; }
-
-        //    public string Name { get; set; }    
-        //    public string Email { get; set; }
-
-        //    public UserInfo(int id, string name, string email)
-        //    {
-        //        Id = id;
-        //        Name = name;
-        //        Email = email;
-        //    }
-        //} 
-
         [HttpPost]
         [Route("login")]
-        public async Task<ActionResult<string>> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+        public async Task<ActionResult<UserWithTokenDto>> Authenticate(AuthenticationRequestBody authenticationRequestBody)
         {
-            //1 : validate the username and password
             var user = await ValidateUserCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
 
             if (user == null)
@@ -66,16 +46,15 @@ namespace UserApplication.Controllers
                 return Unauthorized();
             }
 
-            var securityKey = new SymmetricSecurityKey(
-              Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
-            var signingCredentials = new SigningCredentials(
-                securityKey, SecurityAlgorithms.HmacSha256);
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-            var claimsForToken = new List<Claim>();
-            claimsForToken.Add(new Claim("sub", user.Id.ToString()));
-            claimsForToken.Add(new Claim("given_name", user.Name));
-            claimsForToken.Add(new Claim("email", user.Email));
-           // claimsForToken.Add(new Claim("city", user.City));
+            var claimsForToken = new List<Claim>
+            {
+                new Claim("sub", user.Id.ToString()),
+                new Claim("given_name", user.Name),
+                new Claim("email", user.Email)
+            };
 
             var jwtSecurityToken = new JwtSecurityToken(
                _configuration["Authentication:Issuer"],
@@ -85,21 +64,20 @@ namespace UserApplication.Controllers
                DateTime.UtcNow.AddHours(1),
                signingCredentials);
 
-            var tokenToReturn = new JwtSecurityTokenHandler()
-               .WriteToken(jwtSecurityToken);
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
 
-            return Ok(tokenToReturn);
+            var userDto = _mapper.Map<UserDto>(user);
+
+            return new UserWithTokenDto { Token = tokenToReturn, User = userDto };
         }
 
         private async Task<User> ValidateUserCredentials(string userName, string password)
         {
             var user = await _userRepository.ValidateUserAsync(userName, password);
-
             if (user == null)
             {
                 throw new UnauthorizedAccessException();
             }
-
             return user;
         }
 
@@ -108,11 +86,8 @@ namespace UserApplication.Controllers
         public async Task<ActionResult> CreateUser([FromBody] CreateUserDto createUserDto)
         {
             var userData = _mapper.Map<Entities.User>(createUserDto);
-
             await _userRepository.CreateUserAsync(userData);
-
             await _userRepository.SaveChangesAsync();
-
             return Ok();
         }
     }
---
+++ UserApplication/Models/UserWithTokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace UserApplication.Models
+{
+    public class UserWithTokenDto
+    {
+        public string Token { get; set; }
+        public UserDto User { get; set; }
+    }
+}
